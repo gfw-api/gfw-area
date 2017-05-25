4e0e3d942da93caada27d13dfc4724b2b197c002
Edward Brett <e.j.a.brett@gmail.com>
2017-05-25 17:02:25 +0200

    Fix datasets serializer

--- app/src/models/area.model.js
+++ app/src/models/area.model.js
@@ -2,12 +2,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const Dataset = new Schema({
+    slug: { type: String, required: true, trim: true },
+    active: { type: Boolean, required: true, default: false },
+    startDate: { type: String, required: true, trim: true },
+    endDate: { type: String, required: false, trim: true, default: Date.now }
+})
 const Area = new Schema({
     name: { type: String, required: true, trim: true },
     geostore: { type: String, required: false, trim: true },
     wdpaid: { type: Number, required: false, trim: true },
     userId: { type: String, required: true, trim: true },
-    datasets: { type: Array, required: false, default: [] },
+    datasets: [Dataset],
     createdAt: { type: Date, required: true, default: Date.now },
     image: { type: String, required: false, trim: true }
 });
