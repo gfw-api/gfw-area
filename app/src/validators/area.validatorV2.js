d0599f908b16a987be94441723e7f9ce8658e314
Adam J. Pain <aj.pain89@gmail.com>
2019-05-31 15:36:12 +0200

    Validator fix

--- app/src/validators/area.validatorV2.js
+++ app/src/validators/area.validatorV2.js
@@ -20,7 +20,7 @@ class AreaValidatorV2 {
     static isArray(property) {
         if (property instanceof Array) {
             const invalid = property.filter(str =>
-                typeof(str) !== 'string' ||
+                typeof str !== String ||
                 !str.match(/^[a-zA-Z0-9\u00C0-\u024F\u1E00-\u1EFF_ ]*/g)
             );
             if(invalid.length === 0) return true;
