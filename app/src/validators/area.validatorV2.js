8cdc4480288d1097c6f3d475db888fc4a969fc73
Adam J. Pain <aj.pain89@gmail.com>
2019-09-30 12:00:57 +0200

    Update validator

--- app/src/validators/area.validatorV2.js
+++ app/src/validators/area.validatorV2.js
@@ -72,7 +72,7 @@ class AreaValidatorV2 {
         ctx.checkBody('public').optional().check(pub => AreaValidatorV2.isBool(pub), 'must be boolean');
         ctx.checkBody('fireAlerts').optional().check(pub => AreaValidatorV2.isBool(pub), 'must be boolean');
         ctx.checkBody('deforestationAlerts').optional().check(pub => AreaValidatorV2.isBool(pub), 'must be boolean');
-        ctx.checkBody('webhookUrl').optional().check(webhookUrl => AreaValidatorV2.notEmptyString(status), 'cannot be empty');
+        ctx.checkBody('webhookUrl').optional().check(webhookUrl => AreaValidatorV2.notEmptyString(webhookUrl), 'cannot be empty');
         ctx.checkBody('monthlySummary').optional().check(pub => AreaValidatorV2.isBool(pub), 'must be boolean');
         
         if (ctx.errors) {
