deeaa53d6ff253e963d76eea80ed66a6ab3d40f5
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-02-06 14:11:54 +0000

    Add initial skeleton for subscription service

---
+++ app/src/services/subscription.service.js
@@ -0,0 +1,33 @@
+class SubscriptionsService {
+
+    // TODO: perform request to subscriptions
+    // eslint-disable-next-line no-unused-vars
+    static async getSubscriptionsForUser(queryFilter) {
+        return [];
+    }
+
+    // TODO: perform request to subscriptions
+    // eslint-disable-next-line no-unused-vars
+    static async getSingleSubscription(id) {
+        return {};
+    }
+
+    // TODO: perform request to subscriptions
+    // eslint-disable-next-line no-unused-vars
+    static async createSubscription(data) {
+        return {};
+    }
+
+    // TODO: perform request to subscriptions
+    static async updateSubscription() {
+        return true;
+    }
+
+    // TODO: perform request to subscriptions
+    static async deleteSubscription() {
+        return true;
+    }
+
+}
+
+module.exports = SubscriptionsService;
