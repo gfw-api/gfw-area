4790d7efcb4545be94d71336e970b5bcb8f24c25
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-02-24 09:53:21 +0000

    Replace unnecessary redis dependency with sparkpost email client

--- app/src/services/mail.service.js
+++ app/src/services/mail.service.js
@@ -1,26 +1,46 @@
-const config = require('config');
 const logger = require('logger');
-const redis = require('redis');
+const SparkPost = require('sparkpost');
+const config = require('config');
 
-const CHANNEL = config.get('apiGateway.queueName');
+const API_KEY = config.get('sparkpost.key');
 
 class MailService {
 
-    constructor() {
-        logger.debug('[MailService] Initializing mail queue');
-
-        this.redisClient = redis.createClient({
-            url: config.get('redis.url')
-        });
+    constructor(publicUrl, disableEmailSending = false) {
+        logger.debug('[MailService] Initializing mail service');
+        if (API_KEY) {
+            this.client = new SparkPost(API_KEY);
+        } else {
+            logger.info('Skipping init of Sparkpost client due to missing API key.');
+        }
+        this.publicUrl = publicUrl;
+        this.disableEmailSending = disableEmailSending;
     }
 
     sendMail(template, data, recipients, sender = 'gfw') {
-        this.redisClient.publish(CHANNEL, JSON.stringify({
-            template,
-            data,
+        logger.info('[MailService] Sending confirmation mail to ', recipients);
+        const reqOpts = {
+            substitution_data: { ...data, application: sender },
+            content: { template_id: template },
             recipients,
-            sender
-        }));
+        };
+
+        if (this.disableEmailSending) {
+            logger.info(`[MailService] Email sending disabled, skipping ${template} email`);
+            logger.info(reqOpts);
+            return new Promise((resolve) => resolve());
+        }
+
+        return new Promise((resolve, reject) => {
+            logger.info(reqOpts);
+            this.client.transmissions.send(reqOpts, (error, res) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(res);
+                }
+            });
+        });
     }
 
 }
