e6291e85ebb044045adebc7b281ded29ac4d3170
Tiago Garcia <tiagojsag@gmail.com>
2020-05-12 16:29:50 +0200

    Update mongoose an switch reads to prefer secondary mongo instaces

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,14 @@
+const config = require('config');
+
 const mongooseOptions = {
-    useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    readPreference: 'secondaryPreferred', // Has MongoDB prefer secondary servers for read operations.
+    appname: 'area', // Displays the app name in MongoDB logs, for ease of debug
+    serverSelectionTimeoutMS: 10000, // Number of milliseconds the underlying MongoDB driver has to pick a server
+    loggerLevel: config.get('logger.level') // Logger level to pass to the MongoDB driver
 };
 
 module.exports = mongooseOptions;
