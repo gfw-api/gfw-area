cb201e7cf5f91e952d1ab9dac6eb5c48ad9a7c8e
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-07-03 12:29:29 +0100

    Add confirmed field to areas with the value obtained from the associated subscription

--- app/src/serializers/area.serializerV2.js
+++ app/src/serializers/area.serializerV2.js
@@ -24,6 +24,7 @@ const areaSerializer = new JSONAPISerializer('area', {
         'subscriptionId',
         'email',
         'language',
+        'confirmed',
     ],
     resource: {
         attributes: ['type', 'content']
