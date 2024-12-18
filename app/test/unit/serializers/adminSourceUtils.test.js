311129a8aea92a45090574de3795a063cadc7d59
Gary Tempus Jr <gary.tempus@wri.org>
2024-12-18 15:18:34 -0500

    refactor: moved behavior for adding `source` to its own module
    GTC-3085

---
+++ app/test/unit/serializers/adminSourceUtils.test.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const { addV1SourceForAdministrativeAreas, addV2SourceForAdministrativeAreas } = require('serializers/adminSourceUtils');
+
+const { expect } = chai;
+
+
+
+describe('adminSourceUtils', () => {
+    describe('Adding GADM 2.8 Source Information', () => {
+        const serializedAreaFragment = {
+            attributes: {
+                iso: {
+                    country: 'HND',
+                    region: '8',
+                },
+            },
+        };
+
+        it('should add GADM 2.8 source information', () => {
+            addV1SourceForAdministrativeAreas(serializedAreaFragment);
+            expect(serializedAreaFragment.attributes.iso).to.deep.include({ source: { provider: 'gadm', version: '2.8' } });
+        });
+    });
+
+    describe('Adding GADM 3.6 Source Information', () => {
+        const serializedAreaFragment = {
+            attributes: {
+                iso: {
+                    country: 'HND',
+                    region: '8',
+                    subregion: '4',
+                },
+            },
+        };
+
+        it('should add GADM 3.6 source information', () => {
+            addV2SourceForAdministrativeAreas(serializedAreaFragment);
+            expect(serializedAreaFragment.attributes.iso).to.deep.include({ source: { provider: 'gadm', version: '3.6' } });
+        });
+    });
+});
