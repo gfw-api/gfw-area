27231d9fc3da653846010eef35be7b8926bddb36
Gary Tempus Jr <gary.tempus@wri.org>
2024-12-19 18:09:07 -0500

    :broom: chore: update node to ~20.5 and add missing dev dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 case "$1" in
     develop)
         echo "Running Development Server"
-        exec grunt --gruntfile app/Gruntfile.js | bunyan
+        exec npx grunt --gruntfile app/Gruntfile.js | npx bunyan
         ;;
     startDev)
         echo "Running Start Dev"
